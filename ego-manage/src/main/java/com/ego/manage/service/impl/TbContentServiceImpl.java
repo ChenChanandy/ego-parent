6a491aba028d3ce26168da2a455cf62907967d9b
ChenChan <554771813@qq.com>
2019-05-05 19:10:35 +0800

    前台与后台同步大广告数据

--- ego-manage/src/main/java/com/ego/manage/service/impl/TbContentServiceImpl.java
+++ ego-manage/src/main/java/com/ego/manage/service/impl/TbContentServiceImpl.java
@@ -1,19 +1,30 @@
 package com.ego.manage.service.impl;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.ego.commons.pojo.EasyUIDataGrid;
+import com.ego.commons.utils.JsonUtils;
 import com.ego.dubbo.service.TbContentDubboService;
 import com.ego.manage.service.TbContentService;
 import com.ego.pojo.TbContent;
+import com.ego.redis.dao.JedisDao;
 
 @Service
 public class TbContentServiceImpl implements TbContentService{
 	@Reference
 	private TbContentDubboService tbContentDubboServiceImpl;
+	@Resource
+	private JedisDao jedisDaoImpl;
+	@Value("${redis.bigpic.key}")
+	private String key;
 	
 	@Override
 	public EasyUIDataGrid showContent(long categoryId, int page, int rows) {
@@ -25,7 +36,35 @@ public class TbContentServiceImpl implements TbContentService{
 		Date date = new Date();
 		content.setCreated(date);
 		content.setUpdated(date);
-		return tbContentDubboServiceImpl.insContent(content);
+		int index = tbContentDubboServiceImpl.insContent(content);
+		/**
+		 * 前台和后台大广告数据同步
+		 */
+		//判断redis中是否有缓存数据
+		if(jedisDaoImpl.exists(key)) {
+			String value = jedisDaoImpl.get(key);
+			if(value!=null&&!value.equals("")) {
+				//把Json数据转换成list
+				List<HashMap> list = JsonUtils.jsonToList(value, HashMap.class);
+				HashMap<String,Object> map = new HashMap<>();
+				map.put("srcB", content.getPic2());
+				map.put("height", 240);
+				map.put("alt", "图片加载失败");
+				map.put("width", 670);
+				map.put("src", content.getPic());
+				map.put("widthB", 550);
+				map.put("href", content.getUrl());
+				map.put("heightB", 240);
+				//保证最新六个大广告
+				if(list.size()==6) {
+					list.remove(5);
+				}
+				list.add(0, map);
+				//把list转换成json数据
+				jedisDaoImpl.set(key, JsonUtils.objectToJson(list));
+			}
+		}		
+		return index;
 	}
 
 	@Override
